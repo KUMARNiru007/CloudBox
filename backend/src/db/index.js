45dfcde22e13167803075ff916b80e4a1976c22f
Kumar Nirupam <kumar.nirupam24@gmail.com>
2025-05-05 23:59:10 +0530

    mongoURL changed

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
     }
     
     await mongoose.connect(mongoURI);
-    console.log("Connected to MongoDB via Docker");
+    console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1);
